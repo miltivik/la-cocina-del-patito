b88b3dab9bb804562838f3707dca56d785daa96e
miliS <134473229+miltivik@users.noreply.github.com>
2025-10-28 15:53:06 -0300

    initial commit

---
+++ packages/api/src/context.ts
@@ -0,0 +1,17 @@
+import type { Context as HonoContext } from "hono";
+import { auth } from "@la-cocina-del-patito/auth";
+
+export type CreateContextOptions = {
+	context: HonoContext;
+};
+
+export async function createContext({ context }: CreateContextOptions) {
+	const session = await auth.api.getSession({
+		headers: context.req.raw.headers,
+	});
+	return {
+		session,
+	};
+}
+
+export type Context = Awaited<ReturnType<typeof createContext>>;
